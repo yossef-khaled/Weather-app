f52b79be1f0d4f5b7b2aac67b6eae3ea7c1c8fcb
Yousef Khaled <yossef.k.y333@gmail.com>
2024-08-29 23:45:38 +0300

    Add necessary custom tailwind values

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-color': '#0e2648',
+        'secondary-color': '#2a497b'
+      },
+      padding: {
+        '1rem': '1rem',
+        '2rem': '2rem',
+        '3rem': '3rem',
+        '4rem': '4rem',
+      },
+      margin: {
+        '1rem': '1rem',
+        '2rem': '2rem',
+        '3rem': '3rem',
+        '4rem': '4rem',
+      }
+    },
   },
   plugins: [],
 }
