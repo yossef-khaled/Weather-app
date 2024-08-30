52408faa903c0191be13c93bed8c7ffde5f29d73
Yousef Khaled <yossef.k.y333@gmail.com>
2024-08-30 03:41:20 +0300

    Add consts file for data attributes to be shown & returned from the API

---
+++ src/utils/consts.ts
@@ -0,0 +1,52 @@
+type WeatherConditionElement = {
+    key: string;
+    title: string;
+    scale: string;
+}
+
+export const MAIN_WEATHER_HOURLY_ELEMENTS: WeatherConditionElement[] = [
+    {
+        key: 'tempC',
+        title: 'Temperature in celsius',
+        scale: 'C'
+    },
+    {
+        key: 'windspeedKmph',
+        title: 'Wind speed',
+        scale: 'Km/h'
+    },
+    {
+        key: 'pressure',
+        title: 'Pressure',
+        scale: 'mb'
+    },
+    {
+        key: 'chanceofrain',
+        title: 'Rain chance',
+        scale: '%'
+    },
+    {
+        key: 'uvIndex',
+        title: 'Uv index',
+        scale: '%'
+    },
+
+]
+
+export const SECONDARY_WEATHER_HOURLY_ELEMENTS: WeatherConditionElement[] = [
+    {
+        key: 'tempF',
+        title: 'Temperature in Fahrenheit',
+        scale: 'F'
+    }, 
+    {
+        key: 'cloudcover',
+        title: 'Cloud cover',
+        scale: '%'
+    },
+    {
+        key: 'humidity',
+        title: 'Humidity',
+        scale: '%'
+    }, 
+]
