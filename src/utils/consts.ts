3e06ead237bab3058743e251efebe661ea465840
Yousef Khaled <yossef.k.y333@gmail.com>
2024-09-02 19:17:52 +0300

    Add constants to be used in the chart component

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -51,4 +51,10 @@ export const SECONDARY_WEATHER_HOURLY_ELEMENTS: WeatherConditionElement[] = [
     }, 
 ]
 
-export const HOURLY_SAMPLES__STEP = 100;
+export const HOURLY_SAMPLES_STEP = 100;
+export const TEMPERATURE_READINGS_STEP = 50;
+export const WEATHER_SAMPLES_STARTING_HOUR = 0
+export const WEATHER_SAMPLES_ENDING_HOUR = 2300
+export const PRIMARY_COLOR = '#0e2648'
+export const MEDIUM_GRAY = "#6f7681"
+export const CHART_LINE_WIDTH = 3;
