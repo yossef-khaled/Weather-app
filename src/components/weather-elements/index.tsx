2aa312c87348326e1b75acfcee2bcd2e9e03b2db
Yousef Khaled <yossef.k.y333@gmail.com>
2024-09-02 19:23:02 +0300

    Add inline margin to the card

--- src/components/weather-elements/index.tsx
+++ src/components/weather-elements/index.tsx
@@ -9,7 +9,7 @@ const WeatherElements = ({weather}: WeatherElementsProps) => {
     return (
         <>
             {MAIN_WEATHER_HOURLY_ELEMENTS.filter((MAIN_WEATHER_HOURLY_ELEMENT) => !MAIN_WEATHER_HOURLY_ELEMENT.key.includes('temp')).map((MAIN_WEATHER_HOURLY_ELEMENT, i) => (
-                <Card key={i} classNames='bg-light-gray basis-5/12 flex justify-between text-xl'>
+                <Card key={i} classNames='bg-light-gray basis-5/12 flex justify-between text-xl mx-1'>
                     <span className='text-medium-gray'>{MAIN_WEATHER_HOURLY_ELEMENT.title}</span>
                     <span className='text-medium-gray text-primary-color font-bold'>{weather[MAIN_WEATHER_HOURLY_ELEMENT.key]}{MAIN_WEATHER_HOURLY_ELEMENT.scale}</span>
                 </Card>
