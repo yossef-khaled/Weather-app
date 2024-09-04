a86e5a601ce1c50b04303438f260b033a48e05ca
Yousef Khaled <yossef.k.y333@gmail.com>
2024-09-04 16:28:58 +0300

    Edit weather elements component position in small screens

--- src/components/country-weather-elements/index.tsx
+++ src/components/country-weather-elements/index.tsx
@@ -12,9 +12,9 @@ interface WeatherElementsProps {
 const CountryWeatherElements = ({weather, alertMessage}: WeatherElementsProps) => {
 
     return (
-        <div className='pt-20 w-full'>
+        <div className='pt-20 w-10/12 flex flex-col items-center'>
             <p className='text-4xl font-bold'>Weather elements</p>
-            <div className='max-w-xl overflow-x-auto flex pt-24 px-14 gap-4'>
+            <div className='w-full shrink max-w-2xl overflow-x-auto flex pt-24 px-14 gap-4'>
                 {[...MAIN_WEATHER_HOURLY_ELEMENTS, ...SECONDARY_WEATHER_HOURLY_ELEMENTS].filter((MAIN_WEATHER_HOURLY_ELEMENT) => !MAIN_WEATHER_HOURLY_ELEMENT.key.includes('temp')).map((MAIN_WEATHER_HOURLY_ELEMENT, i) => (
                     <Card key={i} classNames='card text-md inline-block hover:-translate-y-1/4 hover:scale-150 transition ease-in duration-300'>
                         <div className='h-full flex flex-col justify-evenly items-center'>
