c36e6f7c41387f1a7d5a1eb141f36216c258d0ca
Yousef Khaled <yossef.k.y333@gmail.com>
2024-09-06 16:02:33 +0300

    Handel country weather elements scroll with buttons

--- src/components/country-weather-elements/index.tsx
+++ src/components/country-weather-elements/index.tsx
@@ -1,8 +1,11 @@
+import {useRef} from 'react';
 import { MAIN_WEATHER_HOURLY_ELEMENTS, SECONDARY_WEATHER_HOURLY_ELEMENTS } from '../../utils/consts';
 import Card from '../../widgets/Card';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faChevronRight, faChevronLeft} from '@fortawesome/free-solid-svg-icons'
 import './style.css'
 import WeatherALert from '../../widgets/WeatherAlert';
+import { useScrollHorizontally } from '../../utils/hooks';
 
 interface WeatherElementsProps {
     weather: any;
@@ -11,9 +14,16 @@ interface WeatherElementsProps {
 
 const CountryWeatherElements = ({weather, alertMessage}: WeatherElementsProps) => {
 
+    const weatherElementsBarRef = useRef<HTMLDivElement>(null)
+
+    const {scroll, isScrolledToMostLeft, isScrolledToMostRight} = useScrollHorizontally(weatherElementsBarRef)
+
     return (
-        <div className='w-10/12 flex flex-col items-center'>
-            <div className='w-full shrink max-w-2xl overflow-x-auto flex pt-24 px-14 gap-4'>
+        <div className='w-10/12 flex justify-center items-center'>
+            <button onClick={() => scroll('left')} disabled={isScrolledToMostLeft} className='disabled:opacity-5'>
+                <FontAwesomeIcon icon={faChevronLeft} size='2xl'/>
+            </button>
+            <div className='w-full shrink max-w-2xl overflow-x-auto flex pt-24 px-14 gap-4' id="weather-elements-bar" ref={weatherElementsBarRef}>
                 {[...MAIN_WEATHER_HOURLY_ELEMENTS, ...SECONDARY_WEATHER_HOURLY_ELEMENTS].filter((MAIN_WEATHER_HOURLY_ELEMENT) => !MAIN_WEATHER_HOURLY_ELEMENT.key.includes('temp')).map((MAIN_WEATHER_HOURLY_ELEMENT, i) => (
                     <Card key={i} classNames='card text-md inline-block hover:-translate-y-1/4 hover:scale-150 transition ease-in duration-300'>
                         <div className='h-full flex flex-col justify-evenly items-center'>
@@ -26,6 +36,9 @@ const CountryWeatherElements = ({weather, alertMessage}: WeatherElementsProps) =
                     </Card>
                 ))}
             </div>
+            <button onClick={() => scroll('right')} disabled={isScrolledToMostRight} className='disabled:opacity-5'>
+                <FontAwesomeIcon icon={faChevronRight} size='2xl'/>
+            </button>
             {alertMessage ? <WeatherALert alertMessage={alertMessage}/> : null}
         </div>
     )
